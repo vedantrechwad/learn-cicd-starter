d4f9dc8bf5ea04ea9abb3558ce9463b22df13a84
Vedant Rechwad <115932373+vedantrechwad@users.noreply.github.com>
2026-05-27 10:35:25 +0530

    Fix gosec G706 log injection vulnerability by validating port input

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -33,6 +34,12 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+
+	// Validate port is a valid number between 1-65535
+	portNum, err := strconv.Atoi(port)
+	if err != nil || portNum < 1 || portNum > 65535 {
+		log.Fatal("PORT must be a valid port number (1-65535)")
 	}
 
 	apiCfg := apiConfig{}
